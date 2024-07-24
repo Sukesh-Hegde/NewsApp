726f478318614481e5d3488e4fba162bbeac9f39
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-07-24 10:16:16 +0530

    updated page no

--- src/components/News.js
+++ src/components/News.js
@@ -2,23 +2,49 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-
   constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/everything?q=tesla&from=2024-06-23&sortBy=publishedAt&apiKey=dc981be590f84a86a675869e02f8aebf";
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/everything?q=tesla&from=2024-06-23&sortBy=publishedAt&apiKey=dc981be590f84a86a675869e02f8aebf&page=1&pagesize=12";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
-
+    this.setState({ articles: parsedData.articles });
   }
+
+  handleNextClick = async () => {
+    console.log("next");
+    let url = `https://newsapi.org/v2/everything?q=tesla&from=2024-06-23&sortBy=publishedAt&apiKey=dc981be590f84a86a675869e02f8aebf&page=${
+      this.state.page + 1
+    }&pagesize=12`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ page: this.state.page + 1, articles: parsedData.articles });
+  };
+
+  handlePrevClick = async () => {
+    console.log("prev");
+        let url = `https://newsapi.org/v2/everything?q=tesla&from=2024-06-23&sortBy=publishedAt&apiKey=dc981be590f84a86a675869e02f8aebf&page=${
+          this.state.page - 1
+        }&pagesize=12`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+          page: this.state.page -1,
+          articles: parsedData.articles,
+        });
+  };
+
   render() {
     return (
       <div className="container my-3">
@@ -29,19 +55,30 @@ export default class News extends Component {
               <div className="col-md-4" key={element.url}>
                 <NewsItem
                   title={element.title}
-                  description={
-                    element.description 
-                  }
-                  imageUrl={
-                    element.urlToImage
-                      ? element.urlToImage
-                      : "https://ares.shiftdelete.net/2024/07/tesla-megapack-enerji-depolama-fransa-e1721610580883.jpg"
-                  }
+                  description={element.description}
+                  imageUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            onClick={this.handlePrevClick}
+            className="btn btn-dark"
+          >
+            &larr;Previous
+          </button>
+          <button
+            type="button"
+            onClick={this.handleNextClick}
+            className="btn btn-dark"
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
